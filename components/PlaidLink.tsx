44e36b9c8a432c4c6388a3b2eecd8427d7816c23
qgillia <qgillia@gmail.com>
2024-06-15 00:21:13 -0400

    fix PlaidLink open

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -41,7 +41,7 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
       {variant === 'primary' ? (
         <Button 
           className='plaidlink-primary' 
-          onClick={() => open} 
+          onClick={() => open()} 
           disabled={!ready}
         >
           Connect Bank
